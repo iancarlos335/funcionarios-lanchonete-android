e13ab214f08af1be5169e3e694b26e3b739fc681
iancarlos <iancarlos335@gmail.com>
2022-10-31 21:12:26 -0300

    Adicionando mudanças não computadas anteriormente

--- app/src/main/java/com/lanchonete/MainActivity.java
+++ app/src/main/java/com/lanchonete/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             String strvalor = editTextValor.getText().toString();
             Double valor = Double.parseDouble(strvalor); //só vou usar esse cara
 
-            
+
 
 
 
