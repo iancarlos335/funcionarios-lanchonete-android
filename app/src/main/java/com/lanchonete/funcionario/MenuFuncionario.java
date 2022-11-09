be3c7b73f4a961d7c009f896dc5e89fbea031b90
ian-carlos <iancarlos335@gmail.com>
2022-11-09 02:06:16 -0300

    Adicionando configurações, e tentando configurar a bundle que vai pra net

---
+++ app/src/main/java/com/lanchonete/funcionario/MenuFuncionario.java
@@ -0,0 +1,16 @@
+package com.lanchonete.funcionario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lanchonete.R;
+
+public class MenuFuncionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_funcionario);
+    }
+}
