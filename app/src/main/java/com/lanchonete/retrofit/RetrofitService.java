17688cac9353dd54de9a87338cdb2f1958db4af0
Ian Carlos Azevedo Martins <iancarlos335@gmail.com>
2024-01-15 12:36:07 -0300

    Removendo arquivo do .gitignore

---
+++ app/src/main/java/com/lanchonete/retrofit/RetrofitService.java
@@ -0,0 +1,28 @@
+package com.lanchonete.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class RetrofitService {
+
+    private Retrofit retrofit;
+
+    public RetrofitService() {
+        iniciarRetrofit();
+    }
+
+    public void iniciarRetrofit() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.30.25:8080/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson())) //can be another architecture to send data
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+
+}
